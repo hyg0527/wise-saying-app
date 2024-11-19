69d0953cb5212b93937c5246930ff31eaa71aebe
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-11-19 14:10:46 +0900

    App class

---
+++ src/com/example/day1/Main.java
@@ -0,0 +1,17 @@
+package com.example.day1;
+
+class App {
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+
+    }
+}
