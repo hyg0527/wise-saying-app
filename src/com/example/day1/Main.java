f463dd9b1a606a0f425a9120fe894c21cdea4b8a
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-11-19 14:25:35 +0900

    Scanner

--- src/com/example/day1/Main.java
+++ src/com/example/day1/Main.java
@@ -1,10 +1,19 @@
 package com.example.day1;
 
+import java.util.Scanner;
+
 class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        Scanner scanner = new Scanner(System.in);
         System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.printf("입력된 명령어 : %s\n".formatted(cmd));
+
+        scanner.close();
     }
 }
 
@@ -12,6 +21,5 @@ public class Main {
     public static void main(String[] args) {
         App app = new App();
         app.run();
-
     }
 }
