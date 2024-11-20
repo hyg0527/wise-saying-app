999052231c95f865004796a9d4aee27a6596dc10
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-11-20 15:47:25 +0900

    번호 추가

--- src/com/example/day1/Main.java
+++ src/com/example/day1/Main.java
@@ -9,6 +9,8 @@ class App {
 
         Scanner scanner = new Scanner(System.in);
 
+        int id = 1;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -22,7 +24,8 @@ class App {
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+                id++;
             }
             System.out.printf("입력된 명령어 : %s\n".formatted(cmd));
         }
