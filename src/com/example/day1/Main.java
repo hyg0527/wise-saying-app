51e0c586165fdd1af5f53bda4639a5ad9a819e59
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-11-20 15:42:48 +0900

    등록 추가

--- src/com/example/day1/Main.java
+++ src/com/example/day1/Main.java
@@ -16,6 +16,14 @@ class App {
             if (cmd.equals("종료")) {
                 break;
             }
+            else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+
+
+            }
             System.out.printf("입력된 명령어 : %s\n".formatted(cmd));
         }
         scanner.close();
